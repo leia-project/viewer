7e41e713642951958596743ddd7dbf90a317bc40
Bram Huis <bram.huis@sogelink.com>
2025-03-04 15:01:58 +0100

    Added some files to allow support for arcgis legend

---
+++ src/lib/components/map-cesium/LayerControlArcGis/LayerControlArcGis.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let legendUrl: string;
+	console.log("TOOTOTOOIWEHFHIUWEHFIUWHGCUIEGFLWEJKEWGFIUEW");
+</script>
+
+<div>
+	<span>This is the layerinfo for the ArcGis thingy</span>
+	<img id="myImage" src={legendUrl} alt="Base64 Image" />
+</div>
--- src/lib/components/map-cesium/module/layers/arcgis-layer.ts
+++ src/lib/components/map-cesium/module/layers/arcgis-layer.ts
@@ -2,20 +2,48 @@ import type { LayerConfig } from "$lib/components/map-core/layer-config";
 import * as Cesium from "cesium";
 import type { Map } from "../map";
 import { CesiumImageryLayer } from "./imagery-layer";
+import { Debug } from "carbon-icons-svelte";
+import { CustomLayerControl } from "$lib/components/map-core/custom-layer-control";
+import LayerControlArcGis from "../../LayerControlArcGis/LayerControlArcGis.svelte";
 
 export class ArcGISLayer extends CesiumImageryLayer {
-	
+	private layerControl!: CustomLayerControl;
+	private legendUrl: string | undefined;
 	constructor(map: Map, config: LayerConfig) {
 		super(map, config);
 		this.createLayer();
 	}
 
 	async createLayer(): Promise<void> {
-
-		const provider = await Cesium.ArcGisMapServerImageryProvider.fromUrl(this.config.settings["url"], {
-			layers: this.config.settings["layers"],
+		const provider = await Cesium.ArcGisMapServerImageryProvider.fromUrl(this.config.settings.url, {
+			layers: this.config.settings.layers,
 			enablePickFeatures: true
 		});
 		this.source = new Cesium.ImageryLayer(provider, {});
+		this.legendUrl = await this.getLegendUrl();
+		this.addControl();
+	}
+
+	private async getLegendUrl(): Promise<string> {
+		const response = await fetch(`${this.config.settings.url}/legend?f=pjson`);
+		const body = await response.json();
+		const layerLegendInfo = body.layers.find((layer: any) => {
+			return layer.layerId.toString() === this.config.settings.layers;
+		});
+		const srcString = `data:${layerLegendInfo.legend[0].contentType};base64,${layerLegendInfo.legend[0].imageData}`;
+		return srcString;
+	}
+
+	private addControl(): void {
+		this.layerControl = new CustomLayerControl();
+		this.layerControl.component = LayerControlArcGis;
+		this.layerControl.props = {
+			legendUrl: this.legendUrl
+		};
+		this.addCustomControl(this.layerControl);
+	}
+
+	private removeControl(): void {
+		this.removeCustomControl(this.layerControl);
 	}
 }
