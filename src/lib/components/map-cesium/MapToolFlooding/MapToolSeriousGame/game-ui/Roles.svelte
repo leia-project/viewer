66573d4d167467f7cfeb471f749377887d173dbc
Bram Huis <bram.huis@sogelink.com>
2025-03-26 17:48:42 +0100

    Added some svg's for the different roles and added a basic layout for the roles

--- src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/game-ui/Roles.svelte
+++ src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/game-ui/Roles.svelte
@@ -2,16 +2,36 @@
 
 	const roles = [
 		{
+			role: "army",
+			image: "./images/tank-war-svgrepo-com.svg",
+			layersIds: [
+				"2",
+				"3"
+			]
+		},
+		{
 			role: "firefighter",
-			image: "..."
+			image: "./images/fire-station-svgrepo-com.svg",
+			layersIds: [
+				"2",
+				"3"
+			]
 		},
 		{
 			role: "police",
-			image: "..."
+			image: "./images/police-car-svgrepo-com.svg",
+			layersIds: [
+				"2",
+				"3"
+			]
 		},
 		{
-			role: "ambulance",
-			image: "..."
+			role: "GHOR",
+			image: "./images/ambulance-svgrepo-com.svg",
+			layersIds: [
+				"2",
+				"3"
+			]
 		}
 	]
 
@@ -19,14 +39,26 @@
 
 
 <div class="role-menu">
-
+	{#each roles as role}
+		<img src={role.image} alt="{role.role}" width="60%">
+	{/each}
 
 </div>
 
 
 <style>
 	.role-menu {
-		pointer-events: auto;
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		margin-left: 5%;
+		margin-bottom: 1%;
+		pointer-events: all;
+	}
+
+	img {
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
 </style>
