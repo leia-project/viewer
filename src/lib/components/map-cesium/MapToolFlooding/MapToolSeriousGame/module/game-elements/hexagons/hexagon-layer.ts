af78344efb8b0f6377cd2482246b3505d6b81848
Stefan de Graaf <stefan.de.graaf@sogelink.com>
2025-06-10 16:57:46 +0200

    hexagon click event

--- src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/module/game-elements/hexagons/hexagon-layer.ts
+++ src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/module/game-elements/hexagons/hexagon-layer.ts
@@ -90,12 +90,14 @@ export class HexagonLayer {
 		attributes.color = Cesium.ColorGeometryInstanceAttribute.toValue(hexagon.valueToColor(hexagon.population), attributes.color);
 	}
 	
-	
 	public onLeftClick(picked: any): void {
 		let pickedHexagon: Hexagon | undefined;
 		const selectedHexagon = get(this.selectedHexagon);
 		if (picked?.primitive instanceof Cesium.Primitive && picked?.id) {
 			pickedHexagon = this.hexagons.find((hex: Hexagon) => hex.hex === picked.id);
+		}
+		if (!pickedHexagon && picked?.id !== undefined) {
+			return; // Something else was clicked, not a hexagon, so do nothing
 		}
 		if (selectedHexagon && selectedHexagon !== pickedHexagon) {
 			this.unhighlight(selectedHexagon, "click");
@@ -113,7 +115,7 @@ export class HexagonLayer {
 			this.hoveredHexagon = this.hexagons.find((hex: Hexagon) => hex.hex === picked.id);
 			if (this.hoveredHexagon) this.highlight(this.hoveredHexagon, "hover");
 			this.map.viewer.scene.canvas.style.cursor = "pointer";
-		} else {
+		} else if (picked?.id === undefined) {
 			if (this.hoveredHexagon && this.hoveredHexagon !== get(this.selectedHexagon)) {
 				this.unhighlight(this.hoveredHexagon, "hover");
 				this.hoveredHexagon = undefined;
