5a095f9d83bd15cdcbe4cf8d339a500fc2917fa5
Stefan de Graaf <stefan.de.graaf@sogelink.com>
2025-08-27 14:50:09 +0200

    2d/3d mode switcher

--- src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/components/layer-manager/HexagonLayerControl.svelte
+++ src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/components/layer-manager/HexagonLayerControl.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { _ } from "svelte-i18n";
-	import { Slider, Toggle } from "carbon-components-svelte";
+	import { Slider } from "carbon-components-svelte";
 	import type { HexagonLayer } from "../../module/game-elements/hexagons/hexagon-layer";
 	import BaseLayer from "./BaseLayer.svelte";
+	import ViewModeSwitch from "./ViewModeSwitch.svelte";
 
 	export let layer: HexagonLayer;
 
+	const visible = layer?.visible;
 	const use2DMode = layer?.use2DMode;
 	const opacity = layer.alpha;
 
@@ -15,21 +17,15 @@
 <div class="hexagon-layer-control">
 	<div class="hexagon-layer-header">
 		<BaseLayer
-			visible={layer.visible}
+			visible={visible}
 			title={layer.title}
 		/>
-		<div class="toggle-container">
-			<Toggle
-				toggled={!$use2DMode}
-				on:toggle={() => use2DMode.set(!$use2DMode)}
-				hideLabel={true}
-				labelA=""
-				labelB=""
-			/>
-			<span class="toggle-label">
-				{$use2DMode ? "2D" : "3D"}
-			</span>
-		</div>
+		{#if $visible}
+			<div class="toggle-container">
+				<span class="toggle-label">{$use2DMode ? "2D" : "3D"}</span>
+				<ViewModeSwitch mode2D={use2DMode} />
+			</div>
+		{/if}
 	</div>
 	<div class="slider-container">
 		<div class="slider-label">{$_("game.opacity")}</div>
---
+++ src/lib/components/map-cesium/MapToolFlooding/MapToolSeriousGame/components/layer-manager/ViewModeSwitch.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { Writable } from "svelte/store";
+	import { ChoroplethMap, WatsonHealth3DPrintMesh } from "carbon-icons-svelte";
+		
+	export let mode2D: Writable<boolean>;
+
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+<div id="toggle-2d-3d" on:click={() => mode2D.set(!$mode2D)}>
+	<div class="toggle-highlight" style="left: {$mode2D ? '50%' : '0'}"></div>
+	<div class="toggle-option" class:active={!$mode2D}>
+		<WatsonHealth3DPrintMesh />
+	</div>
+	<div class="toggle-option" class:active={$mode2D}>
+		<ChoroplethMap />
+	</div>
+</div>
+
+<style>
+	#toggle-2d-3d {
+		--toggle-size: 40px;
+		display: inline-flex;
+		background-color: rgb(var(--game-color-bg));
+		color: var(--game-color-highlight);
+		border-radius: var(--toggle-size);
+		position: relative;
+		cursor: pointer;
+		border: 1px solid var(--game-color-highlight);
+		height: calc(var(--toggle-size) + 2px);
+	}
+	.toggle-option {
+		width: var(--toggle-size);
+		height: var(--toggle-size);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		
+	}
+	.toggle-highlight {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: var(--toggle-size);
+		height: var(--toggle-size);
+		background-color: var(--game-color-highlight);
+		border-radius: var(--toggle-size);
+		transition: 0.3s;    
+	}
+	
+	:global(#toggle-2d-3d .active svg path) {
+		fill: rgb(var(--game-color-bg)) !important;
+		transition: 0.3s;
+	}
+	
+</style>
