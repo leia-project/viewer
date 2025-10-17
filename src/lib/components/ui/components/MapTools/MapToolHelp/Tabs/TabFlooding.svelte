00d3dca04c946d745b1389ae3384a3eed55a4760
finnp-sogelink <your.email@example.com>
2025-10-17 13:23:19 +0200

    Formatting and small style changes

--- src/lib/components/ui/components/MapTools/MapToolHelp/Tabs/TabFlooding.svelte
+++ src/lib/components/ui/components/MapTools/MapToolHelp/Tabs/TabFlooding.svelte
@@ -3,45 +3,56 @@
     export let base: string; 
 </script>
 
-<div class="img-library">
+
+<div class="img-left">
     <img src="{base}/images/help_flood_open.png" alt="flood icon" />
 </div>
 
 <div class="body-02">
-    {$_("tools.help.flood.description")}<br /><br />
+    {$_("tools.help.flood.description")}
 </div>
 
 <div class="heading-02">{$_("tools.help.flood.headingFloodOpen")}</div>
 <div class="body-02">
-    {$_("tools.help.flood.openFlood")}<br /><br />
+    {$_("tools.help.flood.openFlood")}
 </div>
 
 <div class="heading-02">{$_("tools.help.flood.headingUsingFlood")}</div>
 <div class="body-02">
-    {$_("tools.help.flood.floodBreachDescription")}<br /><br />
+    {$_("tools.help.flood.floodBreachDescription")}
 </div>
 
-<img src="{base}/images/help_flood_breach.png" style="width:100%" alt="breach selection" />
+<div class="img">
+    <img src="{base}/images/help_flood_breach.png" style="width:100%" alt="breach selection" />
+</div>
 
-<br /> <br />
-
-<div class="img-library">
+<div class="img-left">
     <img src="{base}/images/help_flood_menu.png" style="height:500px" alt="flood view" />
 </div>
 
 <div class="body-02">
-    {$_("tools.help.flood.floodMenuDescription")}<br /><br />
+    {$_("tools.help.flood.floodMenuDescription")}
 </div>
 
 <div class="heading-02">{$_("tools.help.flood.headingMeasureDepth")}</div>
 <div class="body-02">
-    {$_("tools.help.flood.measureDepthDescription")}<br /><br />
+    {$_("tools.help.flood.measureDepthDescription")}
 </div>
 
+
 <style>
-    .img-library {
-        float: right;
+    [class*="img"] {
         border: 1px solid var(--cds-ui-03);
-        margin-left: var(--cds-spacing-05);
+        margin-bottom: var(--cds-spacing-03);
+        margin-top: var(--cds-spacing-03);
     }
+
+    .img-left {
+        width: fit-content;
+    }
+
+    .body-02 {
+        margin-bottom: var(--cds-spacing-05);
+    }
+
 </style>
