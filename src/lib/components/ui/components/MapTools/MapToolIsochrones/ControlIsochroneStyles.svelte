1f9bb6a22d08d749e8f5407ab00137a145c4021f
finnp-sogelink <your.email@example.com>
2026-01-21 16:19:14 +0100

    Improved styling

--- src/lib/components/ui/components/MapTools/MapToolIsochrones/ControlIsochroneStyles.svelte
+++ src/lib/components/ui/components/MapTools/MapToolIsochrones/ControlIsochroneStyles.svelte
@@ -8,7 +8,7 @@
 
     const isochrones = isochronesLayer.isochrones;
 
-    
+
     onMount(() => {
         console.log("ControlIsochroneStyles mounted");
 
@@ -22,19 +22,35 @@
     
 </script>
 
+<div class="sliders">
+    {#each $isochrones as isochrone, i}
+        <Slider 
+            labelText={`Fraction ${isochrone.props.isochroneEnd} min Isochrone: ${isochrone.props.weight.toFixed(2)}`} 
+            hideTextInput 
+            min={0} 
+            max={1} 
+            step={0.01} 
+            bind:value={isochrone.props.weight}
+            disabled={isochrone.props.index !== 1}
+        />
+    {/each}
+</div>
 
-{#each $isochrones as isochrone, i}
-    <Slider 
-        labelText={`Fraction ${isochrone.props.isochroneEnd} min Isochrone: ${isochrone.props.weight.toFixed(2)}`} 
-        hideTextInput 
-        min={0} 
-        max={1} 
-        step={0.01} 
-        bind:value={isochrone.props.weight}
-        disabled={isochrone.props.index !== 1}
-    />
-{/each}
 
 <style>
+    .sliders {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 10px auto;
+        padding: 10px;
+        background-color: var(--cds-ui-01);
+        border-radius: 5px;
+        border: 1px solid var(--cds-ui-03);
+        max-width: fit-content;
+	}
 
+    .sliders:empty {
+        display: none;
+    }
 </style>
