94eaaac65d466700d34ee22abf8bbdb92a24dd66
witte-sogelink <witte.verheul@sogelink.com>
2026-06-11 14:44:17 +0200

    Added new scenario names

--- src/lib/components/map-cesium/MapToolFlooding/MapToolFlooding.svelte
+++ src/lib/components/map-cesium/MapToolFlooding/MapToolFlooding.svelte
@@ -100,7 +100,7 @@
 							<div class="info-content">
 								<Dropdown
 									label={scenario}
-									items={$activeBreach.properties.scenarios.map((sc) => ({ id: sc, text: "1:" + sc }))}
+									items={$activeBreach.properties.scenarios.map((sc) => ({ id: sc, text: sc + "mm in 48 uur"}))}
 									bind:selectedId={$selectedScenario}
 									titleText={scenario}
 								/>
