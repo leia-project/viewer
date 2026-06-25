16c5bc9399901b592e25c5679f541d1a2c67dfd2
finnp-sogelink <your.email@example.com>
2026-06-25 16:41:13 +0200

    Fixed lingering point bug when exiting measure mode

--- src/lib/components/tools/MapToolCesiumMeasure/MapToolCesiumMeasure.svelte
+++ src/lib/components/tools/MapToolCesiumMeasure/MapToolCesiumMeasure.svelte
@@ -70,9 +70,13 @@
 	};
 
 	selectedTool.subscribe((selected: MapToolMenuOption) => {
-		if (selected !== tool && get(activeMeasurement)) {
+		if (selected === tool) return;
+
+		if (get(activeMeasurement)) {
 			get(activeMeasurement)?.hideEdit();
 			activeMeasurement.set(undefined);
+		} else {
+			deactivate();
 		}
 	});
 
@@ -133,7 +137,9 @@
 		cesiumMap.off("mouseMove", moveHandle);
 
 		removeMovingPoint();
-		cesiumMap.options.animate.set(previouseAnimateState);
+		if (previouseAnimateState !== undefined) {
+			cesiumMap.options.animate.set(previouseAnimateState);
+		}
 		edittingId.set(undefined);
 
 		cesiumMap.refresh();
