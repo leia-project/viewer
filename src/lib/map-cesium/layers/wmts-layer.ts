f523c98af1fdbc104bf7540584e2ab81d6e815a1
finnp-sogelink <your.email@example.com>
2026-07-14 16:39:23 +0200

    Add WMS bounding box zoom button. Refactors getcapabilities parsing for the styleswitcher, so the code is more concise

--- src/lib/map-cesium/layers/wmts-layer.ts
+++ src/lib/map-cesium/layers/wmts-layer.ts
@@ -7,6 +7,7 @@ export class WmtsLayer extends CesiumImageryLayer {
 
 	constructor(map: Map, config: LayerConfig) {
 		super(map, config);
+		this.setBoundingBoxCameraPosition("wmts");
 	}
 
 	createLayer() {
